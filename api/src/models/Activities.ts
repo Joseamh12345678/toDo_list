f05fe3d807d523f166e7fef6118ca65e79e326e9
Jose <utm23090705@utma.edu.mx>
2024-11-15 15:51:37 -0600

    actualizacon

---
+++ api/src/models/Activities.ts
@@ -0,0 +1,36 @@
+import { model, Schema } from "mongoose"
+
+
+interface IActivities{
+    title:string,
+    dateEnd:Date,
+    description:string,
+    status:"activo" | "pendiente",
+    idUser:Schema.Types.ObjectId | string
+}
+const ActivitySchema = new Schema<IActivities>({
+    title:{
+        type:String,
+        required:true
+    },
+    dateEnd:{
+        type:Date,
+        required:true
+    },
+    description:{
+        type:String,
+        required:true
+    },
+    status:{
+        type:String,
+        required:true,
+        enum:["active","pending"]
+    },
+    idUser:{
+        type:Schema.Types.ObjectId,
+        required:true,
+        ref:"users"
+    }
+
+});
+export const UseModel = model<IActivities>('Activities',ActivitySchema)
---
+++ api/src/models/Users.ts
@@ -0,0 +1,28 @@
+import { model, Schema } from "mongoose"
+
+
+interface IUser{
+    name:string
+    email:string
+    password:string
+    rol:string
+}
+const UserSchema = new Schema<IUser>({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    rol:{
+        type:String,
+        required:true
+    }
+});{timestamps:true}
+export const UseModel = model<IUser>('users',UserSchema)
